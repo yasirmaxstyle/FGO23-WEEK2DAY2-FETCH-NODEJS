720cf8b8b7414f8c92da7598ad97ce9139754aa2
yasirmaxstyle <yasirmu77@gmail.com>
2025-02-19 21:06:45 +0700

    revisi pokemon
    berhasil tapi prosesnya ya begitu

--- pokemon.js
+++ pokemon.js
@@ -11,9 +11,12 @@ const getDataPokemon = async () => {
         }
         const dataJSON = await response.json();
         const dataPokemon = dataJSON.results;
-        
+        const pokemonNames = dataPokemon.map(el => el.name).reduce((acc, item) => {
+            acc.push({name: item});
+            return acc
+        }, []);
 
-        const getDataAbilities = async (url) => {
+        const getDataAbilities = async (url, i) => {
             try {
                 const request = new Request(url, {
                     method: 'GET',
@@ -23,7 +26,7 @@ const getDataPokemon = async () => {
                     throw new Error(response.statusText);
                 }
                 const dataJSON = await response.json();
-                return dataPokemon[0].abilities = dataJSON.abilities;
+                return pokemonNames[i].abilities = dataJSON.abilities;
                 
             } catch (error) {
                 if (error instanceof Error) console.log(error.message)
@@ -31,29 +34,19 @@ const getDataPokemon = async () => {
         }
 
         // fetch part2
-        
-        const fetchAb1 = await getDataAbilities('https://pokeapi.co/api/v2/pokemon/1/');
-        dataPokemon[0].abilities = fetchAb1;
-        const fetchAb2 = await getDataAbilities('https://pokeapi.co/api/v2/pokemon/2/');
-        dataPokemon[1].abilities = fetchAb2;
-        const fetchAb3 = await getDataAbilities('https://pokeapi.co/api/v2/pokemon/3/');
-        dataPokemon[2].abilities = fetchAb3;
-        const fetchAb4 = await getDataAbilities('https://pokeapi.co/api/v2/pokemon/4/');
-        dataPokemon[3].abilities = fetchAb4;
-        const fetchAb5 = await getDataAbilities('https://pokeapi.co/api/v2/pokemon/5/');
-        dataPokemon[4].abilities = fetchAb5;
-        const fetchAb6 = await getDataAbilities('https://pokeapi.co/api/v2/pokemon/6/');
-        dataPokemon[5].abilities = fetchAb6;
-        const fetchAb7 = await getDataAbilities('https://pokeapi.co/api/v2/pokemon/7/');
-        dataPokemon[6].abilities = fetchAb7;
-        const fetchAb8 = await getDataAbilities('https://pokeapi.co/api/v2/pokemon/8/');
-        dataPokemon[7].abilities = fetchAb8;
-        const fetchAb9 = await getDataAbilities('https://pokeapi.co/api/v2/pokemon/1/');
-        dataPokemon[8].abilities = fetchAb9;
-        const fetchAb10 = await getDataAbilities('https://pokeapi.co/api/v2/pokemon/10/');
-        dataPokemon[9].abilities = fetchAb10;
 
-        console.log(dataPokemon);
+        const fetchAb1 = await getDataAbilities('https://pokeapi.co/api/v2/pokemon/1/', 0);
+        const fetchAb2 = await getDataAbilities('https://pokeapi.co/api/v2/pokemon/2/', 1);
+        const fetchAb3 = await getDataAbilities('https://pokeapi.co/api/v2/pokemon/3/', 2);
+        const fetchAb4 = await getDataAbilities('https://pokeapi.co/api/v2/pokemon/4/', 3);
+        const fetchAb5 = await getDataAbilities('https://pokeapi.co/api/v2/pokemon/5/', 4);
+        const fetchAb6 = await getDataAbilities('https://pokeapi.co/api/v2/pokemon/6/', 5);
+        const fetchAb7 = await getDataAbilities('https://pokeapi.co/api/v2/pokemon/7/', 6);
+        const fetchAb8 = await getDataAbilities('https://pokeapi.co/api/v2/pokemon/8/', 7);
+        const fetchAb9 = await getDataAbilities('https://pokeapi.co/api/v2/pokemon/1/', 8);
+        const fetchAb10 = await getDataAbilities('https://pokeapi.co/api/v2/pokemon/10/', 9);
+
+        console.log(pokemonNames);
 
     } catch (error) {
         if (error instanceof Error) console.log(error.message)
