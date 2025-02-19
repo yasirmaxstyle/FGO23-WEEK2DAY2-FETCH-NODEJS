942481692d4cb987c9c04946e8a815cb5b2589f5
yasirmaxstyle <yasirmu77@gmail.com>
2025-02-19 22:07:45 +0700

    WEEK2DAY2 - revisiIndex.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const getDataWeather = async () => {
+import printWithFormat from "./src/formattedPrint.js";
+
+const getDataWeather = async (city) => {
     try {
         const apiKey = '7653323927149d06f4ad598d492dbbee';
-        const url = `https://api.openweathermap.org/data/2.5/weather?q={London}&appid={${apiKey}}`;
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
         const request = new Request(url, {
             method: 'GET',
         });
@@ -10,10 +12,12 @@ const getDataWeather = async () => {
             throw new Error(response.statusText);
         }
         const dataJSON = await response.json();
-        console.log(dataJSON);
+
+        printWithFormat('The weather in %s today is %s, %s', city, dataJSON.weather[0].main, dataJSON.weather[0].description);
+
     } catch (error) {
         if (error instanceof Error) console.log(error.message)
     }
 }
 
-getDataWeather()
+getDataWeather('Bogor');
